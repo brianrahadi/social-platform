89794d1d5761a15cd3908cfd18355fd84b434583
Brian Rahadi <brian.rahadi@gmail.com>
2024-12-20 20:01:50 -0800

    init structure

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/brianrahadi/social-platform
+
+go 1.23.3
+
+require github.com/go-chi/chi/v5 v5.2.0 // indirect)
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ internal/env/env.go
@@ -0,0 +1,33 @@
+package env
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+func GetString(key, fallback string) string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return fallback
+	}
+	val := os.Getenv(key)
+	return val
+}
+
+func GetInt(key string, fallback int) int {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return fallback
+	}
+	val := os.Getenv(key)
+	valAsInt, err := strconv.Atoi(val)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return valAsInt
+}
